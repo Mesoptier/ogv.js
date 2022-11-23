a1a59568fbd1a4182fd029b2f612097d748bcc40
Koen Klaren <koen.klaren@gmail.com>
2022-11-23 14:21:16 +0100

    Add basic types for OGVPlayer and OGVLoader

--- types/index.d.ts
+++ types/index.d.ts
@@ -6,10 +6,72 @@ declare class OGVCompatClass {
 
 export const OGVCompat: OGVCompatClass;
 
-export class OGVPlayer {
+declare class OGVJSElement extends HTMLElement {}
+
+interface OGVPlayerOptions {
+    base?: string;
+    worker?: unknown;
+    webGL?: unknown;
+    forceWebGL?: unknown;
+    stream?: unknown;
+
+    audioContext?: AudioContext;
+    audioDestination?: AudioNode;
+    audioBackendFactory?: unknown;
+
+    // Experimental pthreads multithreading mode, if built.
+    threading?: unknown;
+    // Experimental SIMD mode, if built.
+    simd?: unknown;
+
+    debug?: boolean;
+    debugFilter?: RegExp;
+}
+
+declare class OGVPlayer extends OGVJSElement {
+    constructor(options: OGVPlayerOptions);
+
+    src: string;
+    readonly buffered: OGVTimeRanges;
+    readonly seekable: OGVTimeRanges;
+    currentTime: number;
+    readonly duration: number;
+    readonly paused: boolean;
+    readonly ended: boolean;
+    readonly seeking: boolean;
+    muted: boolean;
+    poster: string;
+    readonly videoWidth: number;
+    readonly ogvjsVideoFrameRate: number;
+    readonly ogvjsAudioChannels: number;
+    readonly ogvjsAudioSampleRate: number;
+    width: number;
+    height: number;
+    autoplay: boolean;
+    controls: boolean;
+    loop: boolean;
+    crossOrigin: string | null;
+    readonly currentSrc: string | null;
+    readonly defaultMuted: boolean;
+    readonly defaultPlaybackRate: number;
+    readonly error: OGVMediaError | null;
+    preload: string;
+    readonly readyState: number;
+    readonly networkState: number;
+    playbackRate: number;
+    readonly played: OGVTimeRanges;
+    volume: number;
+
+    load(): void;
+    canPlayType(type: string): '' | 'probably' | 'maybe';
+    play(): void;
+    pause(): void;
+    fastSeek(time: number): void;
 }
 
 export class OGVLoader {
+    base: string;
+    // TODO: Are the other methods on this class public?
 }
 
 declare enum OGVMediaErrorCode {
